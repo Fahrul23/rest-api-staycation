fa5e02af99c344968fcc9acd4359a7089b58937d
fahrul23 <ihsanfahrul23@gmail.com>
2021-04-16 13:53:48 +0700

    create view bank

--- routes/admin.js
+++ routes/admin.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 router.get('/dashboard',adminController.viewDashboard);
 router.get('/category',adminController.viewCategory);
+router.get('/bank',adminController.viewBank);
 
 module.exports = router;
